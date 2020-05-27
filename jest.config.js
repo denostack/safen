9c7c4acd647ff8aa5de2646b47868ec0be1adeef
Changwan Jun <wan2land@gmail.com>
2020-05-27 11:22:51 +0900

    Create jest.config.js (move from package.json)

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  testRegex: '[^/]*\\.test.tsx?$',
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+  ],
+}
